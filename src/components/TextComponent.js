0c33c413aea79ffb7f3ab73fb9d30f3cad9817a0
Luke Marsh <luke@theappbusiness.com>
2015-07-01 09:27:37 +0100

    added text editor

--- src/components/TextComponent.js
+++ src/components/TextComponent.js
@@ -1,17 +1,53 @@
 'use strict';
 
 var React = require('react/addons');
+var EditableDiv = require('react-wysiwyg-editor');
 
 
 require('styles/TextComponent.sass');
 
 var TextComponent = React.createClass({
 
+  handleContentChange: function(e) {
+    this.setState({copy: e.target.value});
+  },
+
+  toggleEditableDiv: function() {
+    this.setState({
+      editableDivVisible: this.state.editableDivVisible ? false : true
+    });
+  },
+
+  getInitialState: function() {
+    return {
+      editableDivVisible : false,
+      copy: this.props.copy
+    };
+  },
+
   render: function () {
-    var copy = this.props.copy;
+    var editorStyle = {
+      overflow: 'auto',
+      height: 'auto'
+    };
+    var editableDivStyle = {
+      display: this.state.editableDivVisible ? 'block' : 'none'
+    };
+    var copyStyles = {
+      display: this.state.editableDivVisible ? 'none' : 'block'
+    };
 
     return (
-        <div className='copy' dangerouslySetInnerHTML={{__html: copy}} />
+        <div>
+          <div style={editableDivStyle}>
+            <EditableDiv style={editorStyle} content={this.state.copy} onChange={this.handleContentChange} />
+            <div className='text-right downloadButtons'>
+              <button type='submit' className='btn btn-default' onClick={this.toggleEditableDiv}>Cancel</button>
+              <button type='submit' className='btn btn-primary btn-orange' onClick={this.toggleEditableDiv}>Update</button>
+            </div>
+          </div>
+          <div style={copyStyles} onClick={this.toggleEditableDiv} className='copy' dangerouslySetInnerHTML={{__html: this.state.copy}} />
+        </div>
       );
   }
 });
